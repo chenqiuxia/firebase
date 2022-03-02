49a99e140f98384e2f7b065f1c17a51cb9b6d81d
xiaochenchen <xiaochenchen@gmail.com>
2022-03-02 14:16:07 +0800

    新增一些基础的prettier

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -1,54 +1,74 @@
-import React, { useState } from 'react';
-import { Form, Input, Button, Checkbox } from 'antd'
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-import './email.css'
-const Email = props => {
-    const onFinish = values => {
-        console.log('onFinish', values)
-        // const citiesCol = collection(db, 'cities');
-    }
+import React, { useState } from "react";
+import { Form, Input, Button, Checkbox } from "antd";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import "./email.css";
+const Email = (props) => {
+  const onFinish = (values) => {
+    const { email, password } = values;
+    console.log("onFinish", values);
+    // const citiesCol = collection(db, 'cities');
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user, "user");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const onFinishFailed = () => {
-        console.log('onFinishFailed')
-    }
+  const onFinishFailed = () => {
+    console.log("onFinishFailed");
+  };
 
-    return <div className='container'>
-
-        <Form
-            name="basic"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
+  return (
+    <div className="container">
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="email"
+          name="email"
+          rules={[{ required: true, message: "Please input your email!" }]}
         >
-            <Form.Item
-                label="email"
-                name="email"
-                rules={[{ required: true, message: 'Please input your email!' }]}
-            >
-                <Input type='email' />
-            </Form.Item>
+          <Input type="email" />
+        </Form.Item>
 
-            <Form.Item
-                label="Password"
-                name="password"
-                rules={[{ required: true, message: 'Please input your password!' }]}
-            >
-                <Input.Password />
-            </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-            <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-                <Checkbox>Remember me</Checkbox>
-            </Form.Item>
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
 
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
-            </Form.Item>
-        </Form>
-    </div >
-} 
-export default Email
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+export default Email;
