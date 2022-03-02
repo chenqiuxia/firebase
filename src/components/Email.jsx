30a9bb2fb152a1b8bbfdca9d51835db30509ed34
xiaochenchen <xiaochenchen@gmail.com>
2022-03-02 13:22:15 +0800

    新增firebase的配置

---
+++ src/components/Email.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Form, Input, Button, Checkbox } from 'antd'
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import './email.css'
+const Email = props => {
+    const onFinish = values => {
+        console.log('onFinish', values)
+        // const citiesCol = collection(db, 'cities');
+    }
+
+    const onFinishFailed = () => {
+        console.log('onFinishFailed')
+    }
+
+    return <div className='container'>
+
+        <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
+            <Form.Item
+                label="email"
+                name="email"
+                rules={[{ required: true, message: 'Please input your email!' }]}
+            >
+                <Input type='email' />
+            </Form.Item>
+
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+                <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
+    </div >
+} 
+export default Email
--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as firebase from 'firebase/app';
+const firebaseConfig = {
+  apiKey: "AIzaSyB0aNdbzCm-h0vHPdsX2SO5YxpexdhGIJc",
+  authDomain: "init-project-a3acb.firebaseapp.com",
+  projectId: "init-project-a3acb",
+  storageBucket: "init-project-a3acb.appspot.com",
+  messagingSenderId: "251520211314",
+  appId: "1:251520211314:web:193dd500717d488fd2a5eb",
+  measurementId: "G-GXK0DKPQ0B"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 ReactDOM.render(
   <React.StrictMode>
     <App />
